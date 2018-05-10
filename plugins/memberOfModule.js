5c1fa3442fda645bfa214a903ee1a6da8c558f67
pavel.mash <7467039+pavelmash@users.noreply.github.com>
2018-05-10 10:11:52 +0300

    memberOfModule plugin will attach README.md to the modules main page (instead of description)

--- plugins/memberOfModule.js
+++ plugins/memberOfModule.js
@@ -1,7 +1,12 @@
+const fs = require('fs')
+const path = require('path')
+const parse = require('jsdoc/util/markdown').getParser()
 /**
- * This plugin adds a module name specified in custom tag
- * `memberOfModule` to the `memberOf` tag value.
+ * This plugin adds a module name specified in custom tag `memberOfModule` to the `memberOf` tag value.
  * Problem solved here is WebStorm code insight. WebStorm do not understand module:.... syntax
+ *
+ * In addition it will attach README.md to the modules main page (instead of description)
+ *
  * @module plugins/memberOfModule
  * @author Pavel Mash <pavel.mash@gmail.com>
  */
@@ -16,6 +21,15 @@ exports.handlers = {
       memberOfModule = doclet.tags.find(function (tag) {return tag.title === 'memberofmodule'})
       if (memberOfModule) {
         doclet.memberof = 'module:' + memberOfModule.value + '~' + memberof
+      }
+    }
+    // for a main module file attach readme
+    if (doclet.kind === "module" && !doclet.memberof && (doclet.readme === undefined) && doclet.meta.path) {
+      let readmePath = path.join(doclet.meta.path, 'README.md')
+      if (fs.existsSync(readmePath)) {
+        console.log('add readme', readmePath, 'to module', doclet.name)
+        doclet.readme = parse(fs.readFileSync(readmePath, 'utf-8'))
+        delete doclet.description
       }
     }
   }
